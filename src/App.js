b027aec5588cbcc33007be8d550d82cdf1963a1d
Rajesh Kumar <95562281+rajeshcse2123@users.noreply.github.com>
2023-05-11 23:47:06 +0530

    added App.css  added App.js added App.test.js added index.js added reportWebVitals.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import WeText from './components/WeText';
+
+function App() {
+  return (
+    <div className="App">
+        <WeText/>
+    </div>
+  );
+}
+
+export default App;
